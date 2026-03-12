9f5c1e470bf829fc7d7023dd4e35ec0da5bbb179
jobkepin <job.kepin@gmail.com>
2026-03-12 09:50:22 -0700

    updating our directory component to include category items

--- src/App.js
+++ src/App.js
@@ -1,43 +1,12 @@
+import x from "./components/categories/categories.model.js"
+import Directory from "./components/directory/directory.component.jsx";
+
+
 const App = () => {
+
   return (
-    <div className='categories-container'>
-      <div className='category-container'>
-        {/* <img /> */}
-        <div className='category-body-container'>
-          <h2>Hats</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
-      <div className='category-container'>
-        {/* <img /> */}
-        <div className='category-body-container'>
-          <h2>Jackets</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
-      <div className='category-container'>
-        {/* <img /> */}
-        <div className='category-body-container'>
-          <h2>Sneakers</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
-      <div className='category-container'>
-        {/* <img /> */}
-        <div className='category-body-container'>
-          <h2>Womens</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
-      <div className='category-container'>
-        {/* <img /> */}
-        <div className='category-body-container'>
-          <h2>Mens</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
-    </div>
+    <Directory categories={x}/>
   );
-};
+}
 
 export default App;
